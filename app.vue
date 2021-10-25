796c7993d310974e118a0bfe5d26004583e6d5bb
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-10-25 15:28:53 +0300

    initial repo added

---
+++ app.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="container">
+		<div>
+			<img alt="Vue logo" src="./assets/primevue-logo.png">
+			<Toast />
+			
+			<div >
+				<form @submit.prevent="greet">
+					<InputText type="text" v-model="text"/>
+					<Button type="submit" label="Submit"/>
+				</form>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useToast } from "primevue/usetoast";
+
+const text = ref();
+const toast = useToast();
+const greet = () => {
+	toast.add({severity: 'info', summary: 'Hello '  + text.value});
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  margin-top: 1rem;
+  }
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,9 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+export default defineNuxtConfig({
+    css: [
+        'primevue/resources/themes/saga-blue/theme.css',
+        'primevue/resources/primevue.css',
+        'primeicons/primeicons.css'
+    ]
+})
